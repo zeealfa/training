d3773a35bb5ce32eeb7bbe0902ee96fc00b053c2
Zee <zee.alfa@gmail.com>
2015-05-20 05:39:41 +0700

    upgrades

---
+++ js/EmployeeView.js
@@ -0,0 +1,13 @@
+var EmployeeView = function() {
+    this.initialize = function() {
+        this.el = $('<div/>');
+    };
+    this.render = function() {
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+    this.initialize();
+
+}
+
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 var app = {
     initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
+    },
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
     },
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -14,6 +29,7 @@ var app = {
     },
     registerEvents: function() {
         var self = this;
+        $(window).on('hashchange', $.proxy(this.route, this));
         // Check of browser supports touch events...
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
             // ... if yes: register touch event listener to change the "selected" state of the item
@@ -32,6 +48,7 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
+
     }
 };
 
