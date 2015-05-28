e8d8c9642741dd340493956dcf329bd393abda05
Zee <zee.alfa@gmail.com>
2015-05-29 02:22:39 +0700

    Update DB

--- js/main.js
+++ js/main.js
@@ -1,60 +1,25 @@
-var app = {
-    initialize: function() {
-        var self = this;
-//        this.detailsURL = /^#employees\/(\d{1,})/;
-//        this.registerEvents();
-//
-//        this.store = new MemoryStore(function() {
-//            self.route();
-//        });
+function init() {
+    document.addEventListener('deviceready', deviceready, true);
+}
 
+var db;
 
-    }
-/*    ,
-    route: function() {
-        var hash = window.location.hash;
-        if (!hash) {
-            $('body').html(new HomeView(this.store).render().el);
-            return;
-        }
-        var match = hash.match(app.detailsURL);
-        if (match) {
-            this.store.findById(Number(match[1]), function(employee) {
-                $('body').html(new EmployeeView(employee).render().el);
-            });
-        }
-    },
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
-    registerEvents: function() {
-        var self = this;
-        $(window).on('hashchange', $.proxy(this.route, this));
-        // Check of browser supports touch events...
-        if (document.documentElement.hasOwnProperty('ontouchstart')) {
-            // ... if yes: register touch event listener to change the "selected" state of the item
-            $('body').on('touchstart', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('touchend', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-        } else {
-            // ... if not: register mouse events instead
-            $('body').on('mousedown', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('mouseup', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-        }
+function deviceready(){
+    db = window.openDatabase('sikad', '1.0.0', 'Sikad', 1000000);
+    db.transaction(setup, errorHandler, dbReady);
+}
 
-    }
-    */
-};
+function setup(tx){
+    tx.executeSql('CREATE TABLE IF NOT EXISTS t_config(ID_SIKAD INTEGER PRIMARY KEY AUTOINCREMENT, NOMORINDUK VARCHAR(25), LASTACCESS TIMESTAMP)');
+}
 
-app.initialize();
+function errorHandler(e)
+{
+    alert(e.message);
+}
+function dbReady()
+{
+    $('#menu-toggler').on('click',function() {
+        alert('tes');
+    })
+}
